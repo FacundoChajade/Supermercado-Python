89f4f725661a62cb437c8f4da413c9afd6278e93
FacundoChajade <facundo.chajadeet36@gmail.com>
2025-07-08 13:02:22 -0300

    arreglo en las consultas de base de datos y prueba en el main

--- src/DAL/pedidoDAO.py
+++ src/DAL/pedidoDAO.py
@@ -5,14 +5,14 @@ class PedidoDAO():
     def guardarPedido(self,pedido):
         self.conexion.conectar()
         cursor = self.conexion.conn.cursor()
-        cursor.execute("INSERT INTO pedido (cliente) VALUES (&s)",(pedido.cliente_id))
+        cursor.execute("INSERT INTO pedido (cliente) VALUES (%s)",(pedido.cliente_id))
         pedido.id = cursor.lastrowid
         if pedido.id:
-            print(pedido.id)
+            print("id:",pedido.id)
         else:
             print("No se consiguió la id del pedido")
-        for tupla in pedido.listaProducto:
-            cursor.execute("INSERT INTO producto_has_pedidos (producto_id,pedido_id,cantidad_pedida,cantidad_recibida) VALUES (&s,&s,&s)",(tupla[0].get_id(),pedido.id,tupla[2],tupla[1]))
+        for tupla in pedido.listaProductos:
+            cursor.execute("INSERT INTO producto_has_pedido (producto_id,pedido_id,cantidad_pedida,cantidad_recibida) VALUES (%s,%s,%s,%s)",(tupla[0].get_id(),pedido.id,tupla[2],tupla[1]))
             pedido.precioTotal += tupla[0].get_precio() * tupla[1]
-        cursor.execute("UPDATE pedido SET precioFinal=&s WHERE id = &s",(pedido.precioTotal,pedido.id))
+        cursor.execute("UPDATE pedido SET precioFinal=%s WHERE id = %s",(pedido.precioTotal,pedido.id))
         self.conexion.desconectar()
